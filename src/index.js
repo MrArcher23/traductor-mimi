a2fa405d83698a1a845b137b65d2d6d444fc3fbc
MrArcher23 <rgh2309@gmail.com>
2021-12-30 17:41:09 -0600

    add button to copy to clipboard 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import About from "./components/About";
-import App from "./components/pruebaComponent"
+
 
 
 ReactDOM.render(<About />, document.getElementById("app"));
